345bced320e356d264cd1b59b0f67eaf611cb3bd
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-17 14:51:28 +0100

    Create a form component for updating a new movie

---
+++ client/src/Movies/MovieForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MovieForm() {
+
+    return (
+        <form>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+        </form>
+    )
+}
+
+export default MovieForm;
