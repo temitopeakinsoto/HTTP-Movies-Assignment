06956eb123876fe8ab22b087a475466ca719d662
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-18 12:03:09 +0100

    wip

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-function MovieForm() {
+function MovieForm(props) {
+    const movieId = props.match.params.id
+    console.log("this props is", movieId);
+
+    useEffect(() => {
+    axios.get(`http://localhost:5000/api/movies/${movieId}`)
+      .then(res => console.log("this data is", res.data ))
+      .catch(err => console.log(err.response));
+    })
 
     return (
         <form>
-            <input type="text"/>
-            <input type="text"/>
-            <input type="text"/>
+            <div>
+                <input type="text" />
+            </div>
+            <div>
+                <input type="text" />
+            </div>
+            <div>
+                <input type="text" />
+            </div>
+            
+            
         </form>
     )
 }
